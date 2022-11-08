011f3b962c70a5eae10599ac13dac860f662710d
kate9871 <kate.clarke@ucdconnect.ie>
2022-11-08 11:25:19 +0000

    First roll comparison
    Need to figure out for loop details, ie, if player 1 rolls highest and moves checker, for loop for getting command should get player 2 command first

--- Backgammon/src/Game/Game.java
+++ Backgammon/src/Game/Game.java
@@ -7,34 +7,58 @@ public class Game {
 		Board board = new Board();
 		Player[] players = new Player[2];
 		View view = new View();
+		view.displayWelcome();
 		players[0] = new Player(view.getName());
 		players[1] = new Player(view.getName());
-		view.displayWelcome();
+		
 		Command command = null;  //WHY DO I NEED NULL
 		
 		do {
 			//Command command;
 			view.displayBoard(board, players[0], players[1]);
 			boolean commandDone = false;
+			
+			
+			
+			
+			// Gets players first roll
+			int player1roll = view.displayFirstRoll(players[0], Dice.getRoll());
+			int player2roll = view.displayFirstRoll(players[1], Dice.getRoll());
+			
+			if (player1roll > player2roll) {
+				players[0].move(player1roll, 0, board);
+				System.out.println(players[0] + " starts the game!");
+			}
+			else if (player1roll < player2roll) {
+				players[1].move(player2roll,  0, board);
+				System.out.println(players[1] + " starts the game!");
+			}
+			else System.out.println("Players rolled the same number. Roll again!\n");
+		
+			// Need to add in whoever starts moves their checker (move function)
+			// Player who lost first roll is going to be asked their command first
+			// Need to edit for loop
+			
+			
 			for(int i=0; i<=1; i++){
 			
-			do {
-				command = view.getUserInput(players[i]); //issue with printing player name
-				if (command.isRoll()) {
-					players[i].move(Dice.getRoll(),Dice.getRoll(),board);
-					//int roll1 = Dice.getRoll();
-					//int roll2 = Dice.getRoll();
-					view.displayMove(players[i], players[i].getRolls());
-					commandDone = true;
-				} else if (command.isQuit()) {
-					view.displayQuit();
-					commandDone = true;	
+				do {
+					command = view.getUserInput(players[i]); //issue with printing player name
+					if (command.isRoll()) {
+						players[i].move(Dice.getRoll(),Dice.getRoll(),board);
+						//int roll1 = Dice.getRoll();
+						//int roll2 = Dice.getRoll();
+						view.displayMove(players[i], players[i].getRolls());
+						commandDone = true;
+					} else if (command.isQuit()) {
+						view.displayQuit();
+						commandDone = true;	
+					}
+					
 				}
-				
-			}
-			while (!commandDone);
-			if (command.isQuit()) 
-				break;
+				while (!commandDone);
+				if (command.isQuit()) 
+					break;
 			}
 			
 			
--- Backgammon/src/Game/View.java
+++ Backgammon/src/Game/View.java
@@ -14,7 +14,7 @@ public class View {
 
 	public void displayWelcome()
 	{
-		System.out.println("Welcome to Backgammon");
+		System.out.println("Welcome to Backgammon\n");
 	}
 	
 	public String getName () {
@@ -51,7 +51,7 @@ public class View {
 	
 	public void displayPlayer( Player player)
 	{
-		System.out.println("Player: " + player.getName());
+		System.out.println("\nPlayer: " + player.getName());
 		System.out.println("Pips: " + player.getPips());
 		System.out.println("Score: " + player.getScore());
 		
@@ -66,7 +66,7 @@ public class View {
 		displayPlayer(player2);
 		
 		System.out.println("--------------------------------------------------------------------------------------------");
-		System.out.println("|  13  |  14  |  15  |  16  |  17  |  18  |       |  19  |  20  |  21  |  22  |  23  |  24  | ");
+		System.out.println("|  13  |  14  |  15  |  16  |  17  |  18  |       |   19  |  20  |  21  |  22  |  23  |  24  | ");
 		System.out.println("---------------------------------------------------------------------------------------------");
 		
 		for (int i=0;i<6;i++)
@@ -112,7 +112,7 @@ public class View {
 		
 		System.out.println("--------------------------------------------------------------------------------------------");
 		System.out.println("|  12  |  11  |  10  |   9  |   8  |   7  |       |   6  |   5  |   4  |   3  |   2  |   1  | ");
-		System.out.println("--------------------------------------------------------------------------------------------");
+		System.out.println("--------------------------------------------------------------------------------------------\n");
 	}
 	
 	
@@ -126,6 +126,10 @@ public class View {
 		System.out.println("Quit");
 	}
 	
+	public int displayFirstRoll (Player player, int roll) {
+		System.out.println(player.toString() + " first rolls: " + roll);
+		return roll;
+	}
 	
 	
 	
