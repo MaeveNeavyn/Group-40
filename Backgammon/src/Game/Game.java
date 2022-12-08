6db73820aeceb028e22b16f0d436cf3af53e6a0e
kate9871 <kate.clarke@ucdconnect.ie>
2022-12-08 16:32:38 +0000

    Dice input

--- Backgammon/src/Game/Game.java
+++ Backgammon/src/Game/Game.java
@@ -137,6 +137,10 @@ public class Game {
 						// command not done, want player to do something, either roll or quit
 
 					}
+					else if (command.isDice() ) {
+						rolls.add(command.getDice1());
+						rolls.add(command.getDice2());
+					}
 				}
 				while (!commandDone);
 				if (command.isQuit())
