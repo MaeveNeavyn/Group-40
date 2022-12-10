6555be20c957cf6a04a1690889a162f9fc767857
MaeveNeavyn <maeve.neavyn@ucdconnect.ie>
2022-12-10 15:50:25 +0000

    Made CommandTest Class

---
+++ Backgammon/src/Game/CommandTest.java
@@ -0,0 +1,69 @@
+package Game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CommandTest {
+
+	@Test
+	void testCommand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsQuit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsRoll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsPip() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsHint() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsDice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsMove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsTestFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsValid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDice1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDice2() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFileName() {
+		fail("Not yet implemented");
+	}
+
+}
